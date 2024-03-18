bcb5e7b6eb0592d0dfa2b0f6e104e22eac2a7da6
Pedro Valente Toledo <pedrovalentetoledo123@gmail.com>
2024-03-18 19:46:39 -0300

    Javascript Register Validation

--- js/register.js
+++ js/register.js
@@ -1,7 +1,27 @@
-const coco = document.getElementById("teste");
-document.onload = function () {
-  coco.addEventListener("click", pedro());
-  coco.addEventListener("click", function () {
-    console.log(burro);
-  });
-};
+const formRegister = document.getElementById("registerForm");
+const paragrafo = document.createElement("p");
+const mensagem = document.getElementById("mensagem");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const senhaConfirmar = document.getElementById("senhaconfirmar");
+formRegister.addEventListener("submit", function (coquinho) {
+  coquinho.preventDefault();
+  validaCampos();
+});
+function validaCampos() {
+  if (nome.value === "" || email.value === "" || senha.value === "" || senhaConfirmar.value === "") {
+    paragrafo.innerText = "Todos campos devem ser preenchidos!";
+    mensagem.appendChild(paragrafo);
+  } else if (senha.value != senhaConfirmar.value) {
+    paragrafo.innerText = "As senhas devem conferir";
+    mensagem.appendChild(paragrafo);
+  } else {
+    paragrafo.innerText = "Cadastro Realizado com sucesso!";
+    mensagem.appendChild(paragrafo);
+    nome.value = "";
+    email.value = "";
+    senha.value = "";
+    senhaConfirmar.value = "";
+  }
+}
