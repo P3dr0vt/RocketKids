c20d2b7601dbfb491bf15695a56e2b7bc337ac00
Pedro Valente Toledo <pedrovalentetoledo123@gmail.com>
2024-03-18 18:20:50 -0300

    Fiz só cagada aqui

---
+++ js/register.js
@@ -0,0 +1,7 @@
+const coco = document.getElementById("teste");
+document.onload = function () {
+  coco.addEventListener("click", pedro());
+  coco.addEventListener("click", function () {
+    console.log(burro);
+  });
+};
