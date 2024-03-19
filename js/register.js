9295fdded4a438c1c15f39ed476cf37be88e225c
Pedro Valente Toledo <pedrovalentetoledo123@gmail.com>
2024-03-19 10:22:27 -0300

    Terminado JS do registro

--- js/register.js
+++ js/register.js
@@ -1,32 +1,61 @@
 const formRegister = document.getElementById("registerForm");
-const formObject = {};
-const campos = formRegister.querySelectorAll("[name]");
-const paragrafo = document.createElement("p");
-const mensagem = document.getElementById("mensagem");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const senha = document.getElementById("senha");
 const senhaConfirmar = document.getElementById("senhaconfirmar");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
 formRegister.addEventListener("submit", function (coquinho) {
   coquinho.preventDefault();
   validaCampos();
 });
-function validaCampos() {
+
+function LimpaCampos() {
+  nome.value = "";
+  email.value = "";
+  senha.value = "";
+  senhaConfirmar.value = "";
+  checkbox1.checked = false;
+  checkbox2.checked = false;
+}
+function criaMensagem(texto) {
+  const paragrafo = document.createElement("p");
+  const mensagem = document.getElementById("mensagem");
+  paragrafo.innerText = texto;
+  mensagem.appendChild(paragrafo);
+}
+function criaFormObjeto() {
+  const formObject = {};
+  const campos = formRegister.querySelectorAll("[name]");
+  campos.forEach((input) => {
+    formObject[input.getAttribute("name")] = input.value;
+  });
+  return formObject;
+}
+function criaObjetoJson(formObject) {
+  const JsonForm = JSON.stringify(formObject);
+  return JsonForm;
+}
+async function validaCampos() {
   if (nome.value === "" || email.value === "" || senha.value === "" || senhaConfirmar.value === "") {
-    paragrafo.innerText = "Todos campos devem ser preenchidos!";
-    mensagem.appendChild(paragrafo);
-  } else if (senha.value != senhaConfirmar.value) {
-    paragrafo.innerText = "As senhas devem conferir";
-    mensagem.appendChild(paragrafo);
+    criaMensagem("Todos campos devem ser preenchidos!");
+  } else if (senha.value !== senhaConfirmar.value) {
+    criaMensagem("As senhas devem conferir");
+  } else if (!checkbox1.checked) {
+    criaMensagem("Você deve concordar com os termos para prosseguir!");
   } else {
-    campos.forEach((campo) => {
-      formObject[campo.getAttribute("name")] = campo.value;
+    const formObject = criaFormObjeto();
+    const JsonForm = criaObjetoJson(formObject);
+    const request = await fetch("/php/register.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JsonForm,
     });
-    paragrafo.innerText = "Cadastro Realizado com sucesso!";
-    mensagem.appendChild(paragrafo);
-    nome.value = "";
-    email.value = "";
-    senha.value = "";
-    senhaConfirmar.value = "";
+    console.log(JsonForm);
+    criaMensagem("Cadastro Realizado com sucesso!");
+    LimpaCampos();
+    console.log(formObject);
   }
 }
