789863fadaf3417607a11f38aca9f63b03900b77
Pedro Valente Toledo <pedrovalentetoledo123@gmail.com>
2024-03-19 13:25:58 -0300

    Servidor com o registro de usuário

--- js/register.js
+++ js/register.js
@@ -1,61 +1,61 @@
-const formRegister = document.getElementById("registerForm");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-const senhaConfirmar = document.getElementById("senhaconfirmar");
-const checkbox1 = document.getElementById("checkbox1");
-const checkbox2 = document.getElementById("checkbox2");
-formRegister.addEventListener("submit", function (coquinho) {
+const formRegister = $("#registerForm");
+const nome = $("#nome");
+const email = $("#email");
+const senha = $("#senha");
+const senhaConfirmar = $("#senhaconfirmar");
+const checkbox1 = $("#checkbox1");
+const checkbox2 = $("#checkbox2");
+
+formRegister.on("submit", function (coquinho) {
   coquinho.preventDefault();
   validaCampos();
 });
 
 function LimpaCampos() {
-  nome.value = "";
-  email.value = "";
-  senha.value = "";
-  senhaConfirmar.value = "";
-  checkbox1.checked = false;
-  checkbox2.checked = false;
+  nome.val("");
+  email.val("");
+  senha.val("");
+  senhaConfirmar.val("");
+  checkbox1.prop("checked", false);
+  checkbox2.prop("checked", false);
 }
+
 function criaMensagem(texto) {
-  const paragrafo = document.createElement("p");
-  const mensagem = document.getElementById("mensagem");
-  paragrafo.innerText = texto;
-  mensagem.appendChild(paragrafo);
+  const paragrafo = $("<p>").text(texto);
+  const mensagem = $("#mensagem");
+  mensagem.append(paragrafo);
 }
+
 function criaFormObjeto() {
   const formObject = {};
-  const campos = formRegister.querySelectorAll("[name]");
-  campos.forEach((input) => {
-    formObject[input.getAttribute("name")] = input.value;
+  const campos = formRegister.find("[name]");
+  campos.each(function () {
+    formObject[$(this).attr("name")] = $(this).val();
   });
   return formObject;
 }
+
 function criaObjetoJson(formObject) {
   const JsonForm = JSON.stringify(formObject);
   return JsonForm;
 }
-async function validaCampos() {
-  if (nome.value === "" || email.value === "" || senha.value === "" || senhaConfirmar.value === "") {
+
+function validaCampos() {
+  if (nome.val() === "" || email.val() === "" || senha.val() === "" || senhaConfirmar.val() === "") {
     criaMensagem("Todos campos devem ser preenchidos!");
-  } else if (senha.value !== senhaConfirmar.value) {
+  } else if (senha.val() !== senhaConfirmar.val()) {
     criaMensagem("As senhas devem conferir");
-  } else if (!checkbox1.checked) {
+  } else if (!checkbox1.prop("checked")) {
     criaMensagem("Você deve concordar com os termos para prosseguir!");
   } else {
-    const formObject = criaFormObjeto();
-    const JsonForm = criaObjetoJson(formObject);
-    const request = await fetch("/php/register.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JsonForm,
-    });
-    console.log(JsonForm);
-    criaMensagem("Cadastro Realizado com sucesso!");
-    LimpaCampos();
-    console.log(formObject);
+    enviaServidor();
   }
+}
+function enviaServidor() {
+  const formObject = criaFormObjeto();
+  const JsonForm = criaObjetoJson(formObject);
+  $.post("./php/register.php", JsonForm, function (coquinho) {
+    $("#mensagem").html(coquinho);
+    LimpaCampos();
+  });
 }
---
+++ php/register.php
@@ -0,0 +1,29 @@
+<?php
+include './connection.php';
+
+try {
+    $jsonUser = file_get_contents("php://input");
+    $data = json_decode($jsonUser, true);
+    $name = $data['nome'];
+    $email = $data['email'];
+    $password = $data['senha'];
+
+    $checkstmt = $conn->prepare("SELECT email FROM user WHERE email = ?");
+    $checkstmt->bindParam(1, $email);
+    $checkstmt->execute();
+    $emailExists = $checkstmt->fetch();
+
+    if ($emailExists) {
+        echo 'Este email já está cadastrado!';
+    } else {
+        $stmt = $conn->prepare("INSERT INTO user (name, email, password) VALUES (:name, :email, :password)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+        echo 'Cadastro realizado com sucesso!';
+    }
+} catch (PDOException $e) {
+    echo 'Erro: ' . $e->getMessage();
+}
+?>
